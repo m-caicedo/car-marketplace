eb0a3f7662d8736e4bf334d82dbba0036e44871f
m-caicedo <macaicedo03@gmail.com>
2025-08-24 11:01:17 -0500

    Intregacion de clerk authentication, creacion del componente Header

---
+++ src/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+
+function Home() {
+  return (
+    <div>
+      {/* Header */}
+      <Header/>
+
+      {/* Hero */}
+      
+    </div>
+  )
+}
+
+export default Home
